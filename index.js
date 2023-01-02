58dd7902292e0ad46120bc32e9fa6085745862cb
bony5781 <bhowmikabhinav@gmail.com>
2023-01-02 16:29:59 +0530

    Cors error again

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.use("/images",express.static(path.join(__dirname,"public/images")));
 
 //middleware
 app.use(cors())
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(express.json()) //body parser when you make a post request
 app.use(helmet());
 app.use(morgan("common"))
--- routes/users.js
+++ routes/users.js
@@ -42,10 +42,11 @@ router.delete('/:id', async (req, res) => {
 router.get('/', async (req, res) => {
     const userId = req.query.userId;
     const username = req.query.username;
+    console.log(username);
     try {
         const user = userId
             ? await User.findById(userId)
-            : await User.findOne({ username: username });
+            : await User.findOne({ username : username });
         const { password, updatedAt, ...other } = user._doc;
         res.status(200).json(other);
     } catch (err) {
