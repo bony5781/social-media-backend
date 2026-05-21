4dc1689844d9a048092be856c360593e9095a573
Abhinav Bhowmik <89747136+bony5781@users.noreply.github.com>
2026-05-21 17:25:57 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,23 +1,62 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
+
     userId: {
         type: String,
         required: true
     },
+
     desc: {
         type: String,
         max: 500
     },
+
     img: {
         type: String
     },
+
     like: {
         type: Array,
         default: []
-    }
-}
-    , { timestamps: true }
-);
+    },
 
-module.exports = mongoose.model("Post", PostSchema)
+    comments: {
+        type: [
+            {
+                userId: {
+                    type: String,
+                    required: true
+                },
+
+                username: {
+                    type: String,
+                    required: true
+                },
+
+                profilePicture: {
+                    type: String,
+                    default: ""
+                },
+
+                text: {
+                    type: String,
+                    required: true,
+                    max: 200
+                },
+
+                createdAt: {
+                    type: Date,
+                    default: Date.now
+                }
+            }
+        ],
+        default: []
+    }
+
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("Post", PostSchema);
